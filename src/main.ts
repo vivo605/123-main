efa7004011bc27deaf18e8d3e3fe6ed8d0e37e05
misha <mimimisha2110@gmail.com>
2024-12-11 21:20:16 +0300

    Test Homework

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import './lib'
 
 // import './lesson/3'
-import './home/4/3'
+
+import './home/4/5'
+
+// const array = [1,2,3]
+// const sum = (array: number[], n: number = 0) => {
+//   if (array.length) {
+//     return sum(array.slice(0, -1), array.at(-1)! + n)
+//   }
+//   return n
+// }
+// console.log(sum(array))
