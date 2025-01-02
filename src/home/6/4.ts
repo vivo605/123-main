ec174bf4fb6721fe9892ff9aeb994bbb5b3bde03
misha <mimimisha2110@gmail.com>
2025-01-02 23:59:43 +0300

    Осталось это в нормальный код переделать)

--- src/home/6/4.ts
+++ src/home/6/4.ts
@@ -6,12 +6,37 @@ export{}
 
 // Ножницы — это бросок, который используется реже всего, с вероятностью попадания всего 29,6% в обычной игре в «Камень, ножницы, бумагу».
 
+type Validator = (move: number) => boolean;
+
 const start = input('Вы запустили игру "Камень, ножницы, бумага". Хотите поиграть? (Вводите + или -): ')
 
 if (start == '+'){
   while (true){
     print("Первый игрок выбирает чем ходить")
-    const first = input('Чем ты хочешь походить (Камень, ножницы или бумага): ')
+    const inputNumber = (message: string, validator?: Validator): number => {
+      while (true) {
+        const text = input(message)
+        const n = Number(text)
+        if (text !== '' && isFinite(n) && (validator?.(n) ?? true)) {
+          const randoms = random(29,35)
+          print(randoms)
+          if (randoms <= 32){
+            const computer_move = 'Ножницы'
+            print(computer_move)
+          }
+          else if (randoms >= 35){
+            const computer_move = 'Камень'
+            print(computer_move) 
+          }
+          else{
+            const computer_move = 'Бумага'
+            print(computer_move)
+          }
+        }
+      }
+    }
+    const n1 = inputNumber('Чем ты хочешь походить (1.Камень, 2.ножницы или 3.бумага): ')
+    print(n1)
   }
 }
 else{
