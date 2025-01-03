9ca18a55b6f7d3ba66df2524d70b5c93471ac4b0
misha <mimimisha2110@gmail.com>
2025-01-03 23:06:20 +0300

    Исправлю зватра ))

--- src/home/6/4.ts
+++ src/home/6/4.ts
@@ -10,35 +10,54 @@ type Validator = (move: number) => boolean;
 
 const start = input('Вы запустили игру "Камень, ножницы, бумага". Хотите поиграть? (Вводите + или -): ')
 
+const computer_moves = ():string => {
+  const randoms = random(29,35)
+
+  if (randoms <= 32){
+    const computer_move = 'Ножницы'
+    print(computer_move)
+    return 'Ножницы'
+  }
+  else if (randoms >= 35){
+    const computer_move = 'Камень'
+    print(computer_move) 
+    return 'Камень'
+  }
+  else{
+    const computer_move = 'Бумага'
+    print(computer_move)
+    return 'Бумага'
+  }
+}
+
+const valid = (message: string, validator?: Validator):boolean => {
+  const text = input(message)
+  const n = Number(text)
+  if (text !== '' && isFinite(n) && (validator?.(n) ?? true)) {
+    return true
+  }
+  return false
+}
+
+// Todo - Доделать ход игрока
+const user_move = (message:string):string => {
+  print("Первый игрок выбирает чем ходить")
+}
+
+const fight = () => {
+
+}
+
+// Todo - Доделать ход игры
 if (start == '+'){
   while (true){
-    print("Первый игрок выбирает чем ходить")
-    const inputNumber = (message: string, validator?: Validator): number => {
-      while (true) {
-        const text = input(message)
-        const n = Number(text)
-        if (text !== '' && isFinite(n) && (validator?.(n) ?? true)) {
-          const randoms = random(29,35)
-          print(randoms)
-          if (randoms <= 32){
-            const computer_move = 'Ножницы'
-            print(computer_move)
-          }
-          else if (randoms >= 35){
-            const computer_move = 'Камень'
-            print(computer_move) 
-          }
-          else{
-            const computer_move = 'Бумага'
-            print(computer_move)
-          }
-        }
-      }
-    }
-    const n1 = inputNumber('Чем ты хочешь походить (1.Камень, 2.ножницы или 3.бумага): ')
-    print(n1)
   }
 }
 else{
   print('Жаль. Удачи)')
 }
+
+// Todo - Доделать вызовы функций
+const n1 = user_move('Чем ты хочешь походить (1.Камень, 2.ножницы или 3.бумага): ')
+
+print(valid(n1))
