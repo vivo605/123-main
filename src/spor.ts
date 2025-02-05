0f0361607d638f1ffa5bc60b3adc24c03b80a1ec
Winderton <mimimisha2110@gmail.com>
2025-02-05 22:30:52 +0300

    Ну...

--- src/spor.ts
+++ src/spor.ts
@@ -1,29 +1,12 @@
-export type Validator<N extends number> = (num: number) => num is N;
-
-function isPositive(num: number): num is number {
-  return num > 0;
-}
-
-export const homework = <N extends number>(validator?: Validator<N>) => {
+export const homework = () => {
   while (true) {
-    const randomNum = Math.random() * 100;
-
-    if (validator && validator(randomNum)) {
-      const squaredNum = randomNum * randomNum; 
-
-      if (isPositive(squaredNum)) {
-        console.log('Положительное число:', squaredNum);
-        break;
-      } else {
-        console.error('Не удалось получить положительное число');
-      }
-    } else if (!validator) {
-      if (isPositive(randomNum)) {
-        console.log('Положительное число:', randomNum);
-        break;
-      } else {
-        console.error('Не удалось получить положительное число');
-      }
+    const randomNum = Math.random() * 100
+    const x = randomNum**2 - 10 * randomNum + 29
+    if (x > 0) {
+      console.log('Положительное число:', x , '\n Отрицательное число:', -x);
+      break;
+    } else {
+      console.error('Не  поудалосьлучить положительное число');
     }
   }
 };
